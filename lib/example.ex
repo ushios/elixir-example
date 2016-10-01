c9a48ba1c0b82d9a1fff4ed69bbb8a56d5485412
UshioShugo <ushio.s@gmail.com>
2016-10-01 15:25:34 +0800

    add options.

--- lib/example.ex
+++ lib/example.ex
@@ -1,2 +1,31 @@
 defmodule Example do
+  def main(args) do
+    args |> parse_args |> process
+  end
+
+  def process([]) do
+    IO.puts "No arguments given"
+  end
+
+  def process(options) do
+    if options[:raise] do
+      raise "Hello World from raise"
+    end
+
+    if options[:output] do
+      IO.puts "output: #{options[:output]}"
+    end
+
+    if options[:serve] do
+      IO.puts "serve: #{options[:serve]}"
+    end
+  end
+
+  defp parse_args(args) do
+    {options, _, _} = OptionParser.parse(args,
+      aliases: [o: :output, s: :serve, r: :raise],
+      switches: [serve: :boolean, output: :boolean, raise: :boolean],
+    )
+    options
+  end
 end
